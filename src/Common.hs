0ebfbc807a77cb26b6a8b5c54e8bf3cd19ebf9e0
imkatrinaxx <kspanerio@gmail.com>
2016-12-23 19:30:58 +0800

    FINAL

--- src/Common.hs
+++ src/Common.hs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module Common where
 
 import System.Console.ANSI (Color(Black, Red, Green, Yellow, Blue, Magenta, Cyan, White))
@@ -144,47 +143,3 @@ wildcardInHand hand = any (\c -> isWildcard c) hand
 
 getWildcard :: Hand -> Maybe Card
 getWildcard hand = find (\c -> isWildcard c) hand
-=======
-module Common where
-import System.Console.ANSI (Color(Black, Red, Green, Yellow, Blue, Magenta, Cyan, White))
-
-data Card = Card { color::Color ,value::Value } deriving (Read, Eq)
-
-instance Show Card where
-  show c = show (color c, value c)
-
-instance Ord Card where
-  compare c1 c2 = compare (color c1, value c1) (color c2, value c2)
-
-instance Enum Card where
-  toEnum n = let (v,s) = n `divMod` 5
-             in Card (toEnum v) (toEnum s)
-
-  fromEnum c = fromEnum (value c) * 5 + fromEnum (color c)
-
-data Value = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Nine | Plus2 | Stop | ChDir | Plus4 | ChCol | Dummy
-           deriving (Read, Show, Eq, Ord, Enum)
-
-type Hand = [ Card ]
-type Deck = [ Card ]
-type D_Stack = [ Card ]
-
-data Player = HPlayer { name :: String,
-                        hand :: Hand }
-            | AiPlayer { name :: String,
-                         hand :: Hand }
-            deriving (Show, Eq)
-
-data State = State { players :: [ Player ],
-                     deck :: Deck,
-                     d_stack :: D_Stack }
-
-fullDeck :: Deck
-fullDeck = zeroes ++ (nplicate 2 ncards) ++ (nplicate 4 blacks) where
-  zeroes = [ Card c Zero | c <- [Red .. Blue] ]
-  ncards = [ Card c v | c <- [Red .. Blue], v <- [One .. ChDir] ]
-  blacks = [ Card Black v | v <- [Plus4, ChCol] ]
-
-nplicate :: Int -> [a] -> [a]
-nplicate n xs = concat $ (take n) $ repeat xs
->>>>>>> origin/master
--- src/Shuffler.hs
+++ src/Shuffler.hs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module Shuffler where
 
 import Common
@@ -26,42 +25,3 @@ seed = mkStdGen 1234
 
 updateDeck' :: State -> Deck -> IO State
 updateDeck' gs deck' = return (gs { deck = deck' })
-=======
-module Shuffler where
-
-import Common
-import System.Random
-import System.Random.Shuffle
-
--- TODO: Implement a random shuffling algorithm
-shuffleDeck :: State -> IO State
-shuffleDeck state@State{players = _players, 
-                        deck = _deck} = return state { players = setHnds (shufdeck) 0 4 _players,
-                                                        deck = drop 28 shufdeck}
-                                            where shufdeck = shuffler seed                                
-
-
-shuffler :: (RandomGen gen) => gen -> [Card]
-shuffler = shuffle' fullDeck 108
-
-seed = mkStdGen 1234
-
-setHnds :: Deck -> Int -> Int -> [Player] -> [Player]
-setHnds deck m n [] = []
-setHnds deck m n (p:ps) = p {hand = setCards deck m n} : setHnds deck (m+1) (n+1) ps
-
-setCards :: Deck -> Int -> Int -> Hand
-setCards deck m n = map (deck!!) ids where ids = takeWhile (<28) [m,n..]
-
--- CARD DISTRIBUTION EACH PLAYER
-
--- REFER TO ALJOMAI'S ILLUSTRATION
-
--- 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
--- x 1 2 3 x 5 6 7 x 9 10 11  x 13 14 15  x 17 18 19  x 21 22 23  x 25 26 27
---   x 2 3   x 6 7   x 10 11     x 14 15     x 18 19     x 22 23     x 26 27
---     x 3     x 7      x 11        x 15        x 19        x 23        x 27
---       x       x         x           x          x            x           x
-
--- where x = dealt card to each player simultaenously
->>>>>>> origin/master
